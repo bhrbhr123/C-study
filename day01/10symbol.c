017a0e99d69b9b77e78bd927db491c401dcdb220
Bai <18234287753@163.com>
2023-11-04 20:18:49 +0800

    day01 first submission

---
+++ day01/10symbol.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+    int a = 0x11111111;
+    unsigned int b = 0x11111111;
+
+    printf("a = %d\n", a);
+    printf("b = %d\n", b);
+
+
+
+
+    return 0;
+}
